eb2a9f0384613f4d7c27dbea326dc893fb6192c8
Thais95 <thaiscdaraujo95@gmail.com>
2022-08-06 00:23:47 -0300

    Finishing the script

--- estacionamento.py
+++ estacionamento.py
@@ -41,9 +41,9 @@ class Vaga:
         self.veiculo_estacionado = None
         self.livre = True
         
-    def ocupar(self, placa):
+    def ocupar(self, veiculo):
         if self.livre:
-            self.veiculo_estacionado = placa
+            self.veiculo_estacionado = veiculo
             self.livre = False
             
     def desocupar(self):
@@ -72,33 +72,33 @@ class Estacionamento:
     
     def estacionar_carro(self, carro):
         if carro.estacionado:
-            return print(f'Carro {carro.placa} já está estacionado.')
+            return print(f'Carro [placa {carro.placa}] já está estacionado.')
         
         if self.vagas_livres['Carro'] == 0:
-            return print(f'[Carro {carro.placa}]: Não há vagas disponíveis.')
+            return print(f'Carro [placa {carro.placa}] não estacionou pois não há vagas disponíveis.')
         
         for vaga in self.vagas['Carro']:
             if vaga.livre:
-                vaga.ocupar(carro.placa)
+                vaga.ocupar(carro)
                 carro.estacionar()
                 self.vagas_livres['Carro'] -= 1
-                return print(f'[Carro {carro.placa}]: Foi estacionado na vaga {vaga.id}.')
+                return print(f'Carro [placa {carro.placa}] foi estacionado na vaga {vaga.id}.')
             
     def remover_carro(self, carro):
         if not carro.estacionado:
-            return print(f'Carro {carro.placa} não está estacionado.')
+            return print(f'Carro [placa {carro.placa}] não está estacionado.')
         
         for vaga in self.vagas['Carro']:
             if vaga.veiculo_estacionado == carro.placa:
                 vaga.desocupar()
                 carro.sair_da_vaga()
                 self.vagas_livres['Carro'] += 1
-                return print(f'Carro {carro.placa} saiu da vaga {vaga.id}.')
+                return print(f'Carro [placa {carro.placa}] saiu da vaga {vaga.id}.')
             
     
     def estacionar_moto(self, moto):
         if moto.estacionado:
-            return print(f'Moto {moto.placa} já está estacionado.')
+            return print(f'Moto [placa {moto.placa}] já está estacionada.')
         
         tipo_da_vaga = ''
         
@@ -107,43 +107,77 @@ class Estacionamento:
         elif self.vagas_livres['Carro']:
             tipo_da_vaga = 'Carro'
         else:
-            return print(f'Moto {moto.placa} não estacionou pois não há vagas disponíveis.')
+            return print(f'Moto [placa {moto.placa}] não estacionou pois não há vagas disponíveis.')
         
         for vaga in self.vagas[tipo_da_vaga]:
             if vaga.livre:
-                vaga.ocupar(moto.placa)
+                vaga.ocupar(moto)
                 moto.estacionar(tipo_da_vaga)
                 self.vagas_livres[tipo_da_vaga] -= 1
-                return print(f'[Moto {moto.placa}]: Foi estacionado na vaga {vaga.id}.')
+                return print(f'Moto [placa {moto.placa}] foi estacionada na vaga {vaga.id}.')
         
     def remover_moto(self, moto):
         if not moto.estacionado:
-            return print(f'Moto {moto.placa} não está estacionada.')
+            return print(f'Moto [placa {moto.placa}] não está estacionada.')
         
         for vaga in self.vagas[moto.tipo_da_vaga]:
             if vaga.veiculo_estacionado == moto.placa:
                 self.vagas_livres[moto.tipo_da_vaga] += 1
                 vaga.desocupar()
                 moto.sair_da_vaga()
-                return print(f'Moto {moto.placa} saiu da vaga {vaga.id}.')
+                return print(f'Moto [placa {moto.placa}] saiu da vaga {vaga.id}.')
             
     
+    def __veiculos_estacionados(self):
+        string_carro = string_moto = ''
+        quantidade_carro = quantidade_moto = 0
+        for vaga in (self.vagas['Moto'] + self.vagas['Carro']):
+            if vaga.livre:
+                continue
+            if vaga.veiculo_estacionado.tipo == 'Carro':
+                string_carro += f"Vaga {vaga.id}: Placa {vaga.veiculo_estacionado.placa}\n"
+                quantidade_carro += 1
+            else:
+                string_moto += f"Vaga {vaga.id}: Placa {vaga.veiculo_estacionado.placa}\n"
+                quantidade_moto += 1
+        return string_carro, string_moto, quantidade_carro, quantidade_moto
+    
+    
     def __str__(self):        
-        return f"""[Status do estacionamento]
-Total de vagas livres: {self.vagas_livres['Carro']+self.vagas_livres['Moto']}
+        string_carro, string_moto, quantidade_carro, quantidade_moto = self.__veiculos_estacionados()
+        
+        return f"""
+[Relatório do estacionamento]
+
+Total de vagas ocupadas: {quantidade_carro + quantidade_moto}
+Total de vagas livres: {self.vagas_livres['Carro'] + self.vagas_livres['Moto']}
 Vagas de carro livres: {self.vagas_livres['Carro']}
 Vagas de moto livres: {self.vagas_livres['Moto']}
-"""
+
+Carros estacionados ({quantidade_carro})
+{string_carro}
+Motos estacionadas ({quantidade_moto})
+{string_moto}"""
 
 
+# Criando um lote no estacionamento (quantidade de vagas de carro, quantidade de vagas de moto)
+estacionamento = Estacionamento(5, 5)
 
-carro_azul = Carro('298283')
-estacionamento = Estacionamento(25, 25)
+# Criando um carro, estacionando e removendo da vaga
+carro_azul = Carro('KPB-3944')
 estacionamento.estacionar_carro(carro_azul)
 estacionamento.remover_carro(carro_azul)
-estacionamento.estacionar_carro(carro_azul)
-moto_rosa = Moto('828SJ2')
+
+# Criando um carro e o deixando estacionado
+carro_preto = Carro('KZD-8380')
+estacionamento.estacionar_carro(carro_preto)
+
+# Criando motos e estacionando / removendo
+moto_rosa = Moto('LQD-9413')
+moto_dourada = Moto('KQW-5409')
 estacionamento.estacionar_moto(moto_rosa)
+estacionamento.estacionar_moto(moto_dourada)
 estacionamento.remover_moto(moto_rosa)
 estacionamento.estacionar_moto(moto_rosa)
+
 print(estacionamento)
